361764216a5dfc8b643d24db54ca97f3f96fccad
ARamos <albertramos902@gmail.com>
2024-05-23 10:01:06 +0100

    fixed delete method cors error

--- app.js
+++ app.js
@@ -150,8 +150,10 @@ app.delete('/movies/:id', (req, res) => {
 app.options('/movies', (req, res) => {
   const origin = req.header('origin')
   if (ACCEPTED_ORIGINS.includes(origin) || !origin) {
-     res.header("Access-Control-Allow-Origin", origin)
+    res.header("Access-Control-Allow-Origin", origin)
+    res.header('Access-Control-Allow-Methods', 'GET,POST, PATCH, DELETE')
   }
+  res.send(200)
 })
 
 
